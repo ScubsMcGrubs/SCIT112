7a83b04c75c73e09d944dc5bf5067e7747a5cc34
ScubsMcGrubs <theartfulsimon@gmail.com>
2021-02-01 12:44:10 -0800

    Assignment 6 commit

--- Club/urls.py
+++ Club/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('resources/', views.getresources, name = 'resources'),
+    path('meetings/', views.getmeetings, name = 'meetings'),
+    path('meetingdetail/<int:id>' , views.meetingDetail, name = 'detail'),
 ]
